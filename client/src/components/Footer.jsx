8e2df1d7edac6966fbe4d58f6963c88aaaf03333
Amiir25 <amirsadik636@gmail.com>
2025-07-14 18:22:10 +0300

    Add Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { assets } from '../assets/assets';
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='flex items-center justify-between mt-20 py-5 border-t border-[#023766] px-6 md:px-10'>
+        <Link to={"/"} className='flex flex-col items-center w-fit'>
+            <img src={assets.logo} alt="" className='w-12' />
+            <p className='text-xs text-[#023766] font-black'>BookTracker</p>
+        </Link>
+        <p className='font-medium text-gray-700'>
+            Built by
+            <a href=""> Amir Sadik</a>
+        </p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
