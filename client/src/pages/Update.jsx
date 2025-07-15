a7f003b0289bc4a87009e98c16d765c31c42f041
Amiir25 <amirsadik636@gmail.com>
2025-07-15 23:56:58 +0300

    Update Update.jsx

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -41,6 +41,7 @@ const Update = () => {
         try {
             await axios.put(`http://localhost:5000/books/${id}`, book);
             navigate('/');
+            alert("Book updated successfully");
         } catch (err) {
             console.error(err);
         }
@@ -57,28 +58,28 @@ const Update = () => {
                 {/* Title */}
                 <div className="flex flex-col">
                     <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" placeholder="Add title"
+                    <input type="text" id="title" name="title" value={book.title} placeholder="Add title"
                         className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                 </div>
                 {/* Author */}
                 <div className="flex flex-col">
                     <label htmlFor="author">Author</label>
-                    <input type="text" id="author" name="author" placeholder="Add the author"
+                    <input type="text" id="author" name="author" value={book.author} placeholder="Add the author"
                         className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                 </div>
                 {/* Date */}
                 <div className="flex flex-col">
                     <label htmlFor="date">Published Year</label>
-                    <input type="number" id="date" name="date" placeholder="Enter published year"
+                    <input type="number" id="date" name="date" value={book.date} placeholder="Enter published year"
                         className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                 </div>
                 {/* Pages */}
                 <div className="flex flex-col">
                     <label htmlFor="pages">Number of Pages</label>
-                    <input type="number" id="pages" name="totalpage" placeholder="Enter number of pages"
+                    <input type="number" id="pages" name="totalpage" value={book.totalpage} placeholder="Enter number of pages"
                         className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                 </div>
@@ -86,7 +87,7 @@ const Update = () => {
                 <div className="flex flex-col">
                     <label htmlFor="genreInput">Genre</label>
                     <input type="text" id="genreInput" list="genre" name="genre"
-                        placeholder="Enter the genre" className="border border-gray-400 px-2 py-2 rounded"
+                        value={book.genre} placeholder="Enter the genre" className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                     <datalist id="genre">
                         <option value="Tech"></option>
@@ -98,7 +99,7 @@ const Update = () => {
                 {/* Status */}
                 <div className="flex flex-col">
                     <label htmlFor="statusInput">Status</label>
-                    <input type="text" id="statusInput" list="status" name="status" placeholder="Status"
+                    <input type="text" id="statusInput" list="status" name="status" value={book.status} placeholder="Status"
                         className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                     <datalist id="status">
@@ -110,7 +111,7 @@ const Update = () => {
                 {/* Current Page */}
                 <div className="flex flex-col">
                     <label htmlFor="currentPage">Current Page</label>
-                    <input type="number" id="currentPage" name="currentpage" placeholder="Enter your current pages"
+                    <input type="number" id="currentPage" name="currentpage" value={book.currentpage} placeholder="Enter your current pages"
                         className="border border-gray-400 px-2 py-2 rounded"
                         onChange={handleChange} />
                 </div>
